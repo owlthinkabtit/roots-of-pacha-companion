4fb5ac489f587ff0de64410bdb4cbd40424be9eb
owlthinkabtit <haggins.jameka@gmail.com>
2025-05-05 16:17:32 -0400

    this is my first time creating a mobile app, but here we are. This is a game that I'm very passionate about, and this project came from a simple 'I wonder if there's an app for this'. There isn't so I decided to try and make one. We're using react native, typescript, expo. Working on bare bones things and will try to include detailed commits with this one.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
